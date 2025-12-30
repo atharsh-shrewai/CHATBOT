8885470dcff765444536fb1c5f73290d7d76bbd6
Atharsh K <atharshkrishnamoorthy@gmail.com>
2025-12-30 10:46:00 +0530

    Included nginx and lets encrypt

--- infra/main.tf
+++ infra/main.tf
@@ -34,13 +34,23 @@ resource "aws_security_group" "allow_web" {
   }
 
   ingress {
-    from_port   = 8000
-    to_port     = 8000
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  
   }
 
   ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
